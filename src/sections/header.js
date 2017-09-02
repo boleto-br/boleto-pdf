1075cdf18d934d9b7a22c23d7ad12953ee4de4cf
Rafael Castro <rccastrorafael@gmail.com>
2017-09-01 21:58:18 -0300

    more lines to the table

--- src/sections/header.js
+++ src/sections/header.js
@@ -19,9 +19,16 @@ module.exports = function(doc) {
   const mediumFontSize = 11
   const fontBold = 'fonts/roboto-bold.ttf'
   const fontRegular = 'fonts/roboto-regular.ttf'
-  
+  const value = 'R$ 9,99'
+  const chargeValue = ' '
+
   doc
-    .image('logos/logo-bradesco.jpg', startX + smallGutterX, startY, {height: 23})
+    .image(
+      'logos/logo-bradesco.jpg', 
+      startX + smallGutterX, 
+      startY, 
+      {height: 23}
+    )
 
   doc
     .rect(startX + 120, startY, line, boxHeight)
@@ -33,27 +40,50 @@ module.exports = function(doc) {
     .text('237-2', startX + 130, startY + 8)
 
   doc
-    .rect(startX + 170, startY, line, boxHeight)
+    .rect(
+      startX + 170, 
+      startY, 
+      line, 
+      boxHeight
+    )
     .fill(lineColor)
   
   doc
     .fontSize(largefontSize)
-    .text('23797.50603 91900.000125 08005.429108 7 72670000000999', startX + 190, startY + 8)
+    .text(
+      '23797.50603 91900.000125 08005.429108 7 72670000000999', 
+      startX + 190, 
+      startY + 8
+    )
 
   doc
-    .rect(startX, startY + boxHeight, tableLimit, line)
+    .rect(
+      startX, 
+      startY + boxHeight, 
+      tableLimit, 
+      line
+    )
     .fill(lineColor)
   
   // box
   doc
-    .rect(startX, startY + boxHeight, tableLimit, boxHeight)
+    .rect(
+      startX, 
+      startY + boxHeight, 
+      tableLimit, 
+      boxHeight
+    )
     .lineWidth(line)
     .stroke(lineColor)
   
   doc
     .fontSize(smallFontSize)
     .font(fontRegular)
-    .text('Local de Pagamento', startX + smallGutterX, startY + boxHeight + smallGutterY)
+    .text(
+      'Local de Pagamento', 
+      startX + smallGutterX, 
+      startY + boxHeight + smallGutterY
+    )
 
   doc
     .fontSize(fontSize)
@@ -65,7 +95,12 @@ module.exports = function(doc) {
     )
   
   doc
-    .rect(startX, startY + boxHeight * 2, tableLimit * (2/4), boxHeight)
+    .rect(
+      startX, 
+      startY + boxHeight * 2, 
+      tableLimit * (2/4), 
+      boxHeight
+    )
     .stroke(lineColor)
   
   doc
@@ -90,13 +125,18 @@ module.exports = function(doc) {
     .fontSize(smallFontSize)
     .font(fontBold)
     .text(
-    'Rua Tenete Silveira, 315 - Centro - Florianópolis - SC  - CEP 88010-301',
-    startX + 40,
-    startY + boxHeight * 2 + 15
+      'Rua Tenete Silveira, 315 - Centro - Florianópolis - SC  - CEP 88010-301',
+      startX + 40,
+      startY + boxHeight * 2 + 15
     )
 
   doc
-    .rect(startX + tableLimit * (2 / 4), startY + boxHeight * 2, tableLimit * (1 / 4), boxHeight)
+    .rect(
+      startX + tableLimit * (2 / 4), 
+      startY + boxHeight * 2, 
+      tableLimit * (1 / 4), 
+      boxHeight
+    )
     .stroke(lineColor)
 
   doc
@@ -112,22 +152,26 @@ module.exports = function(doc) {
     .fontSize(fontSize)
     .font(fontRegular)
     .text(
-    '7506-0 / 54291-1',
-    startX + tableLimit * (2 / 4) + 65,
-    startY + boxHeight * 2 + gutterY
+      '7506-0 / 54291-1',
+      startX + tableLimit * (2 / 4) + 65,
+      startY + boxHeight * 2 + gutterY
     )
 
   doc
-    .rect(startX + tableLimit * (3 / 4), startY + boxHeight * 2, tableLimit * (1 / 4), boxHeight)
+    .rect(
+      startX + tableLimit * (3 / 4), 
+      startY + boxHeight * 2, 
+      tableLimit * (1 / 4), boxHeight
+    )
     .stroke(lineColor)
   
   doc
     .fontSize(smallFontSize)
     .font(fontRegular)
     .text(
-    'Data de Vencimento',
-    startX + tableLimit * (3 / 4) + smallGutterX,
-    startY + boxHeight * 2 + smallGutterY
+      'Data de Vencimento',
+      startX + tableLimit * (3 / 4) + smallGutterX,
+      startY + boxHeight * 2 + smallGutterY
     )
 
   doc
@@ -138,4 +182,286 @@ module.exports = function(doc) {
       startX + tableLimit * (3 / 4) + 65,
       startY + boxHeight * 2 + 7
     )
+    
+  // next line table
+  doc
+    .rect(
+      startX, startY + boxHeight * 3, 
+      tableLimit * (1.5 / 10), boxHeight)
+    .stroke(lineColor)
+  
+  doc
+    .fontSize(smallFontSize)
+    .font(fontRegular)
+    .text(
+      'Data do Documento',
+      startX + smallGutterX,
+      startY + boxHeight * 3 + smallGutterY
+    ) 
+
+  doc
+    .fontSize(fontSize)
+    .font(fontRegular)
+    .text(
+      '18/08/2017',
+      startX + gutterX,
+      startY + boxHeight * 3 + gutterY
+    ) 
+  
+  doc
+    .rect(
+      startX + tableLimit * (1.5 / 10) , 
+      startY + boxHeight * 3, tableLimit * (1.5 / 10), boxHeight)
+    .stroke(lineColor)
+
+  doc
+    .fontSize(smallFontSize)
+    .font(fontRegular)
+    .text(
+      'Data do Processamento',
+      startX + tableLimit * (1.5 / 10) + smallGutterX,
+      startY + boxHeight * 3 + smallGutterY
+    ) 
+
+  doc
+    .fontSize(fontSize)
+    .font(fontRegular)
+    .text(
+      '18/08/2017',
+      startX + tableLimit * (1.5 / 10) + gutterX,
+      startY + boxHeight * 3 + gutterY
+    )
+
+  doc
+    .rect(
+      startX + tableLimit * ( 3 / 10), startY + boxHeight * 3, 
+      tableLimit * ( 0.6 / 10), boxHeight)
+    .stroke(lineColor)
+
+  doc
+    .fontSize(smallFontSize)
+    .font(fontRegular)
+    .text(
+      'Carteira',
+      startX + tableLimit * (3 / 10) + smallGutterX,
+      startY + boxHeight * 3 + smallGutterY
+    )
+
+  doc
+    .fontSize(fontSize)
+    .font(fontRegular)
+    .text(
+    '09',
+      startX + tableLimit * (3 / 10) + gutterX,
+      startY + boxHeight * 3 + gutterY
+    )
+  
+  doc
+    .rect(
+      startX + tableLimit * (3.6 / 10),
+      startY + boxHeight * 3, tableLimit * (2 / 10), boxHeight)
+    .stroke(lineColor)
+  
+  doc
+    .fontSize(smallFontSize)
+    .font(fontRegular)
+    .text(
+      'N° documento',
+      startX + tableLimit * (3.6 / 10) + smallGutterX,
+      startY + boxHeight * 3 + smallGutterY
+    )
+  
+  doc
+    .fontSize(fontSize)
+    .font(fontRegular)
+    .text(
+      '42493',
+      startX + tableLimit * (3.6 / 10) + gutterX,
+      startY + boxHeight * 3 + gutterY
+    )
+
+  doc
+    .rect(
+      startX + tableLimit * (5.6 / 10), 
+      startY + boxHeight * 3, tableLimit * (2.2 / 10), boxHeight)
+    .stroke(lineColor)
+  
+  doc
+    .fontSize(smallFontSize)
+    .font(fontRegular)
+    .text(
+    'Nosso Número',
+      startX + tableLimit * (5.6 / 10) + smallGutterX,
+      startY + boxHeight * 3 + smallGutterY
+    )
+
+  doc
+    .fontSize(mediumFontSize)
+    .font(fontBold)
+    .text(
+      '09/19000001208-0',
+      startX + tableLimit * (5.6 / 10) + gutterX,
+      startY + boxHeight * 3 + gutterY
+    )
+
+  doc
+    .rect(
+      startX + tableLimit * (7.8 / 10),
+      startY + boxHeight * 3, tableLimit * (2.2 / 10), boxHeight
+    )
+    .stroke(lineColor)
+  
+  doc
+    .fontSize(smallFontSize)
+    .font(fontRegular)
+    .text(
+      '(=) Valor do documento',
+      startX + tableLimit * (7.8 / 10) + smallGutterX,
+      startY + boxHeight * 3 + smallGutterY
+    )
+
+  doc
+    .fontSize(mediumFontSize)
+    .font(fontBold)
+    
+  const widthStringValue = doc.widthOfString(value)
+  
+  doc
+    .text(
+      value,
+      startX + tableLimit - widthStringValue - gutterX,
+      startY + boxHeight * 3 + 9
+    )
+
+  // new line
+  doc
+    .rect(
+      startX,
+      startY + boxHeight * 4, tableLimit * (1 / 5), boxHeight)
+    .stroke(lineColor)
+  
+  doc
+    .fontSize(smallFontSize)
+    .font(fontRegular)
+    .text(
+      '(-) Desconto / Abatimento',
+      startX +  smallGutterX,
+      startY + boxHeight * 4 + smallGutterY
+    )
+  
+  doc
+    .fontSize(fontSize)
+    .font(fontRegular)
+    .text(
+      ' ',
+      startX + gutterX,
+      startY + boxHeight * 4 + gutterY
+    )
+
+  doc
+    .rect(
+      startX + tableLimit * (1 / 5),
+      startY + boxHeight * 4, tableLimit * (1 / 5), boxHeight)
+    .stroke(lineColor)
+  
+  doc
+    .fontSize(smallFontSize)
+    .font(fontRegular)
+    .text(
+      '(-) Outras deduções',
+      startX + tableLimit * (1 / 5) + smallGutterX,
+      startY + boxHeight * 4 + smallGutterY
+    )
+
+  doc
+    .fontSize(fontSize)
+    .font(fontRegular)
+    .text(
+      ' ',
+      startX + tableLimit * (1 / 5) + gutterX,
+      startY + boxHeight * 4 + gutterY
+    )
+
+  doc
+    .rect(
+      startX + tableLimit * (2 / 5),
+      startY + boxHeight * 4, tableLimit * (1 / 5), boxHeight)
+    .stroke(lineColor)
+  
+  doc
+    .fontSize(smallFontSize)
+    .font(fontRegular)
+    .text(
+      '(+) Mora / Multa',
+      startX + tableLimit * (2 / 5) + smallGutterX,
+      startY + boxHeight * 4 + smallGutterY
+    )
+
+  doc
+    .fontSize(fontSize)
+    .font(fontRegular)
+    .text(
+      ' ',
+      startX + tableLimit * (2 / 5) + gutterX,
+      startY + boxHeight * 4 + gutterY
+    )
+
+  doc
+    .rect(
+      startX + tableLimit * (3 / 5),
+      startY + boxHeight * 4, tableLimit * (1 / 5), boxHeight)
+    .stroke(lineColor)
+  
+  doc
+    .fontSize(smallFontSize)
+    .font(fontRegular)
+    .text(
+    '(+) Mora / Multa',
+      startX + tableLimit * (3 / 5) + smallGutterX,
+      startY + boxHeight * 4 + smallGutterY
+    )
+
+  doc
+    .fontSize(fontSize)
+    .font(fontRegular)
+    .text(
+      ' ',
+      startX + tableLimit * (3 / 5) + gutterX,
+      startY + boxHeight * 4 + gutterY
+    )
+
+  doc
+    .rect(
+      startX + tableLimit * (4 / 5),
+      startY + boxHeight * 4, tableLimit * (1 / 5), boxHeight)
+    .stroke(lineColor)
+
+  doc
+    .fontSize(smallFontSize)
+    .font(fontRegular)
+    .text(
+    '(=) Valor cobrado',
+    startX + tableLimit * (4 / 5) + smallGutterX,
+    startY + boxHeight * 4 + smallGutterY
+    )
+  
+  doc
+    .fontSize(mediumFontSize)
+    .font(fontBold)
+
+  const widthOfStringCharge = doc.widthOfString(chargeValue)
+  
+  doc
+    .text(
+      chargeValue,
+      tableLimit - widthOfStringCharge + gutterX,
+      startY + boxHeight * 4 + gutterY
+    )
+  
+  // payer info box
+  doc
+    .rect(
+      startX,
+      startY + boxHeight * 5, tableLimit, boxHeight * 3)
+    .stroke(lineColor)
 }
